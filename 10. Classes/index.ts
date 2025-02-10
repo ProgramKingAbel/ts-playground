a7ce73fb4731365b8fe652e0cf9811abc372ac32
Abel Morara <abelmorara254@gmail.com>
2025-02-10 19:21:13 +0300

    init classes

---
+++ 10. Classes/index.ts
@@ -0,0 +1,22 @@
+/**
+ * Class Properties Annotations
+ * 
+ * - you can annotate class properties with a type to ensure consistency
+ * 
+ */
+
+class Person {
+    name: string;
+    age: number;
+
+    // readonly properties
+    // readonly name: string;
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+const person = new Person("John", 90);
+console.log(person);
