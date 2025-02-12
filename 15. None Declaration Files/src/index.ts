1e559aaefd44f34cb905c994c7aaaf2ef893d4ef
Abel Morara <abelmorara254@gmail.com>
2025-02-12 14:38:36 +0300

    chore: implement ts in js libraries without declaration files

---
+++ 15. None Declaration Files/src/index.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (request: Request, response: Response) => {
+    response.send("Hello, Typescript with Express!")
+})
+
+app.listen(port,  ()=> console.log(`Server running on port ${port}`))
